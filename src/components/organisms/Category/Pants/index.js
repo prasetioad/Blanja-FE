ea9f3a22646bd3e20649008996717346e00945b1
kevinfaridap <kevinfaridap@gmail.com>
2021-05-08 00:13:05 +0700

    integration home and category page

--- src/components/organisms/Category/Pants/index.js
+++ src/components/organisms/Category/Pants/index.js
@@ -2,21 +2,22 @@ import {useState, useEffect} from 'react'
 import style from './pants.module.css'
 import axios from 'axios'
 import { FaStar } from 'react-icons/fa'
-import {pants} from '../../../images'
+import { useHistory } from 'react-router'
 
 function Pants() {
+  const history = useHistory()
   const [getPants, setGetPants] = useState([])
   // Untuk Banyaknya rating/bintang
   const [rating, setRating] =useState(null)
 
   useEffect(()=>{
-    axios.get(`https://jsonplaceholder.typicode.com/users`)
+    axios.get(`${process.env.REACT_APP_API_URL}/product/category/3`)
     .then((res)=>{
-      const dataAllPants = res.data
+      const dataAllPants = res.data.data
     //   console.log(dataAllPants);
       setGetPants(dataAllPants)
      
-      setRating(5) 
+      setRating(4) 
     })
     .catch((err)=>{
       console.log(err);
@@ -28,30 +29,26 @@ function Pants() {
       <div className="container">
         <p className={style["navigation"]}>Home - Category - Pants</p>
         <p className={style["title"]}>Pants</p>
-      
         <div className="row">
           {getPants !== undefined ? getPants.map((item)=>{
           return (
           <>
           <div className="col-lg-3 col-6 mb-5" >
-            <div className={style["card"]}>
-              <img className={[["card-img-top"], style["product-img"]].join(' ')} src={pants} alt=""/>
+            <div className={style["card"]} onClick={()=>{ history.push('/product')}}>
+              <img className={[["card-img-top"], style["product-img"]].join(' ')} src={`${process.env.REACT_APP_API_IMG}${item.image}`} alt=""/>
               <div className="card-body">
-                <p className={style["product-name"]}>{item.username}</p>
-                <p className={style["price"]}>$ 40.0</p>
-                <p className={style["teks-store"]}>Zalora Cloth</p>
+                <p className={style["product-name"]}>{item.title}</p>
+                <p className={style["price"]}>Rp {item.price}</p>
+                <p className={style["teks-store"]}>{item.brand}</p>
                 {[...Array(5)].map((star, i)=>{
                   const ratingValue = i + 1;
                   return (
                     <>
-                   
                     <FaStar 
                       className={style["star"]} 
                       size={25}
                       color={ratingValue <= (rating) ? '#FFBA49' : '#D4D4D4'}
-                      
                     />
-                   {/* <img className={style["rating"]} src="" alt=""/> */}
                   </>
                   ) 
                 })}
--- src/components/organisms/Home/NewHome/index.js
+++ src/components/organisms/Home/NewHome/index.js
@@ -2,29 +2,31 @@ import { useState, useEffect } from "react";
 import style from "./newhome.module.css";
 import axios from "axios";
 import { FaStar } from 'react-icons/fa'
-import {tshirt} from '../../../images'
 import { useHistory } from 'react-router';
 
 function NewHome() {
-  const [getNewProduct, setGetNewProduct] = useState([]);
   const history = useHistory()
-  // Untuk Banyaknya rating/bintang
+  const [getNewProduct, setGetNewProduct] = useState([]); 
   const [rating, setRating] =useState(null)
 
-
   useEffect(()=>{
-    axios.get(`https://jsonplaceholder.typicode.com/users`)
+    axios.get(`${process.env.REACT_APP_API_URL}/product?perPage=12`)
     .then((res)=>{
-      const dataNewProduct = res.data
+      const dataNewProduct = res.data.data
       // console.log(dataNewProduct);
       setGetNewProduct(dataNewProduct)
       
-      setRating(5) 
+      // Untuk set rating/bintang
+      setRating(4)
     })
     .catch((err)=>{
       console.log(err);
     })
   }, [])
+
+  const changeRating = (param) =>{
+    setRating(param) 
+  } 
 
   return (
     <div className={style.newHomeOuterBackgroundParentDIV}>
@@ -33,31 +35,27 @@ function NewHome() {
        <p className={style["teks"]}>You’ve never seen it before!</p>
        <div className="row">
         {getNewProduct !== undefined ? getNewProduct.map((item)=>{
-        return (
-        <>
+          return (
+            <>
           <div className="col-lg-3 col-6 mb-5" >
             <div className={style["card"]} onClick={()=>{ history.push('./product')}}>
-              <img className={[["card-img-top"], style["product-img"]].join(' ')} src={tshirt} alt=""/>
+              <img className={[["card-img-top"], style["product-img"]].join(' ')} src={`${process.env.REACT_APP_API_IMG}${item.image}`} alt=""/>
               <div className="card-body">
-                <p className={style["product-name"]}>{item.username}</p>
-                <p className={style["price"]}>$ 40.0</p>
-                <p className={style["teks-store"]}>Zalora Cloth</p>
+                <p className={style["product-name"]}>{item.title}</p>
+                <p className={style["price"]}>Rp {item.price}</p>
+                <p className={style["teks-store"]}>{item.brand}</p>
                 {[...Array(5)].map((star, i)=>{
                   const ratingValue = i + 1;
                   return (
                     <>
-                   
                     <FaStar 
                       className={style["star"]} 
                       size={25}
                       color={ratingValue <= (rating) ? '#FFBA49' : '#D4D4D4'}
-                      
                     />
-                   {/* <img className={style["rating"]} src="" alt=""/> */}
                   </>
                   ) 
                 })}
-                {/* <img className={style["rating"]} src="https://www.flaticon.com/svg/vstatic/svg/786/786230.svg?token=exp=1620118115~hmac=a4e96f09c345196119a6f8470b55151f" alt=""/> */}
               </div>
             </div>
           </div>
